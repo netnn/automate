6a507c07d2b6b5822097e0367211bda9e3707bd6
netanel nahmani <nnati9292@gmail.com>
2026-03-24 10:08:22 +0200

    Changed the test page

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -27,7 +27,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     // Run headless in CI, headed locally
-    headless: !!process.env.CI,
+    headless: !!process.env.CI ? true : false,
     
   },
 
--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('www.playwright.dev');
+  await page.goto('https://playwright.dev');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 // test to check the get started link on the homepage
 test('get started link', async ({ page }) => {
-  await page.goto('www.playwright.dev');
+  await page.goto('https://playwright.dev');
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
