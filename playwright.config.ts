d7bc32fe0799bb85fa56bad5f854f4e74626fa3a
netanel nahmani <nnati9292@gmail.com>
2026-03-24 22:23:05 +0200

    all

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  timeout: 120_000, // 2 minutes for each test
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.BASE_URL,
